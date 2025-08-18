10bca41fc94985bcd097d0d9336ec0a4ffa32cc5
manntrr <150742899+manntrr@users.noreply.github.com>
2025-08-18 17:26:20 -0400

    box2

--- CommonObjectLibrary/CommonObjectLibrary/obj/Debug/net9.0/CommonObjectLibrary.AssemblyInfo.cs
+++ CommonObjectLibrary/CommonObjectLibrary/obj/Debug/net9.0/CommonObjectLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonObjectLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98583de645bd3ec5c2107ff13a5426ea500f3ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+507df5b3b3d608809cafca49c02d3821ad7dc6ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonObjectLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonObjectLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
